a71e5f9bf6b5a0089bc190eda5d301d9fcf1b0d0
kenya-sk <ksakka0309@gmail.com>
2020-09-22 18:30:27 +0900

    POL-93: modify minutes card design

--- src/components/minutesCard.js
+++ src/components/minutesCard.js
@@ -7,8 +7,10 @@ export default function MinutesCard(props) {
     return (
         <Link className={styles.card} to={props.to}>
             <div className={styles.header}>
-                <div className={styles.date}>{props.date}</div>
                 <div className={styles.name}>{props.name}</div>
+            </div>
+            <div className={styles.header}>
+                <div className={styles.date}>{props.date}</div>
             </div>
             <ul>
                 {props.topics.map((topic) => {
