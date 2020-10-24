9dca23f618db2952be905d87b528b71c1d57d780
kenya-sk <ksakka0309@gmail.com>
2020-10-24 21:48:25 +0900

    POL-138: add label of diet date

--- src/pages/timelines.js
+++ src/pages/timelines.js
@@ -1,18 +1,34 @@
 import React from "react"
-import {graphql, navigate, Link} from 'gatsby'
+import {graphql, navigate} from 'gatsby'
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import {getTimelinesTitle, getTimelinesDescription} from "../utils/seoutils";
 import {Container} from "../components/container";
 import {buildPath} from "../utils/urlutils";
-import Calendar from 'react-calendar';
-import './calendar.css';
+import {formatDate} from "../utils/formatutils"
+import Calendar from "react-calendar";
+import "./calendar.css";
 
 
-export const formatCalendarDate = (date) => {
-    return String(date.getFullYear()) + String(date.getMonth()+1).padStart(2, '0') + String(date.getDate()).padStart(2, '0')
+export const getDietDate = (Timeline) => {
+    const dietDate = [];
+    Timeline.map((timeline) => {
+        if (timeline.totalMinutes > 0) {
+            dietDate.push(formatDate(timeline.date));
+        }
+    });
+
+    return dietDate;
 }
 
+export const formatCalendarDate = (date) => {
+    return String(date.getFullYear()) + String(date.getMonth()+1).padStart(2, "0") + String(date.getDate()).padStart(2, "0")
+}
+
+export const setDietDate = ({date, view }, dietDate) => {
+    const fDate = String(date.getFullYear()) + "/" + String(date.getMonth()+1).padStart(2, "0") + "/" + String(date.getDate()).padStart(2, "0")
+    return (view === "month" && dietDate.includes(fDate)) ?  "react-calendar-diet-day": null;
+}
 
 export default class App extends React.Component {
     state = {
@@ -21,19 +37,28 @@ export default class App extends React.Component {
 
     onChange = date => this.setState({ date })
 
+    dietDate = getDietDate(this.props.data.politylink.Timeline);
+
     render() {
         return (
             <Layout>
                 <SEO title={getTimelinesTitle()} description={getTimelinesDescription()}/>
                 <div>
-                    <p style={{textAlign: `center`}}>タイムライン</p>
+                    <p style={{textAlign: `center`, fontWeight: `bold`}}>タイムライン</p>
+                    <div style={{textAlign: `right`}}>
+                        <p style={{color: `#00bfff`, fontSize: `0.8em`, display: `inline`}}>■ </p>
+                        <p style={{fontSize: `0.8em`, paddingRight: `15px`, display: `inline`}}>国会開催日</p>
+                    </div>
                     <Container>
                         <Calendar
-                            locale="ja-JP"
+                            locale={"ja-JP"}
+                            calendarType={"US"}
                             onChange={this.onChange}
                             value={this.state.date}
+                            view={"month"}
                             minDate={new Date(2019, 12, 1)}
                             maxDate={new Date()}
+                            tileClassName={({date, view }) => setDietDate({date, view}, this.dietDate)}
                             onClickDay={(value) => navigate(buildPath(`Timeline:${formatCalendarDate(value)}`))}
                         />
                     </Container>
