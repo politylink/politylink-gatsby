391e986f5def1a477187051675d094d50c4c5efa
kenya-sk <ksakka0309@gmail.com>
2020-10-24 16:21:44 +0900

    POL-138: add calender

--- src/pages/timelines.js
+++ src/pages/timelines.js
@@ -1,28 +1,40 @@
 import React from "react"
-import {graphql, Link} from 'gatsby'
+import {graphql, navigate, Link} from 'gatsby'
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import {getTimelinesDescription} from "../utils/seoutils";
-import {formatDateWithDay} from "../utils/formatutils";
 import {Container} from "../components/container";
 import {buildPath} from "../utils/urlutils";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+
+export const formatCalendarDate = (date) => {
+    return String(date.getFullYear()) + String(date.getMonth()+1).padStart(2, '0') + String(date.getDate()).padStart(2, '0')
+}
 
 
 export default class App extends React.Component {
+    state = {
+        date: new Date(),
+    }
+
+    onChange = date => this.setState({ date })
 
     render() {
         return (
             <Layout>
                 <SEO description={getTimelinesDescription()}/>
-                <Container>
-                    <ul>
-                        {this.props.data.politylink.Timeline.map((timeline) => {
-                            const text = `${formatDateWithDay(timeline.date)}
-                            (議案: ${timeline.totalBills}, 会議: ${timeline.totalMinutes}, ニュース: ${timeline.totalNews}) `
-                            return <li><Link to={buildPath(timeline.id)}>{text}</Link></li>;
-                        })}
-                    </ul>
-                </Container>
+                <div>
+                    <Container>
+                        <Calendar
+                            locale="ja-JP"
+                            onChange={this.onChange}
+                            value={this.state.date}
+                            onClickDay={(value) => navigate(buildPath(`Timeline:${formatCalendarDate(value)}`))}
+                        />
+                    </Container>
+                </div>
             </Layout>
         )
     }
@@ -41,3 +53,5 @@ export const query = graphql`
         }
     }
 `
+
+// totalMinutes != 0
