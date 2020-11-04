3506b7b1cc26262ad54dec577c0e5b68996e27b9
lwhb <lawhub.tokyo@gmail.com>
2020-11-04 23:37:17 +0900

    Revert "fix: calendar class name"
    This reverts commit 152bc06a8651eea824a69c53ff83b9668cee3179.

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,10 +7,9 @@ import "./calendar.css";
 
 export const setDietDate = ({date, view}, dietDates) => {
     const fDate = formatJsDate(date, "/")
-    let tileClassName = "";
-    if (view === "month" && dietDates.includes(fDate)) {tileClassName = classNames(tileClassName, "react-calendar-diet-day")}
-    if (date.toDateString() === new Date().toDateString()) {tileClassName = classNames(tileClassName,"react-calendar-today")}
-    return (tileClassName !== "") ? tileClassName : null;
+    return (view === "month" && dietDates.includes(fDate)) ? "react-calendar-diet-day"
+        : (date.toDateString() === new Date().toDateString()) ? "react-calendar-today"
+        : null;
 }
 
 export const TimelineCalendar = props => {
