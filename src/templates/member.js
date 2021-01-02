36bb9c3d611dcc640f8273ba801c92d40efbf9e1
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-01-02 23:45:36 +0900

    POL-204: fix typo

--- src/templates/member.js
+++ src/templates/member.js
@@ -65,7 +65,7 @@ export default function Member({data}) {
                                 minutes={activity.minutes}
                                 urls={activity.urls}
                             />
-                        } else {
+                        } else if (activity.bill != null) {
                             return <BillActivityCard
                                 datetime={activity.datetime}
                                 bill={activity.bill}
