4df522a1c710f6f0fdb60d91610e3792d710bb2a
Toshiyuki Yokoyama <6br@users.noreply.github.com>
2021-01-14 13:35:03 +0900

    Update articles.js

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -20,6 +20,9 @@ export default function Members() {
                     <li>
                         <a href="https://kenya-sk.medium.com/politylink%E3%82%92%E6%B4%BB%E7%94%A8%E3%81%97%E3%81%9F%E5%88%86%E6%9E%90%E3%83%81%E3%83%A5%E3%83%BC%E3%83%88%E3%83%AA%E3%82%A2%E3%83%AB-51700d1e1d7f">PolityLinkを活用した分析チュートリアル</a>
                     </li>
+                    <li>
+                        <a href="https://qiita.com/teo/items/4cf64f06fa105aa12d7e">PolityLink を支える GraphQL テクノロジー</a>
+                    </li>
                 </ul>
             </Container>
         </Layout>
