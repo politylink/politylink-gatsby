fbcfec913e878839f7eb25b9539003b0810d380a
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-03-24 23:30:12 +0900

    POL-273: fix style

--- src/components/activityCard.js
+++ src/components/activityCard.js
@@ -6,10 +6,10 @@ import {Link} from "gatsby";
 import {buildPath} from "../utils/urlutils";
 
 export const MinutesActivityCard = (props) => {
-    return (<p className={styles.minutes_activity}>
+    return (<div className={styles.activity}>
         {formatDate(props.datetime)}
         <div>
-            &nbsp;<FontAwesomeIcon icon="microphone" size="sm"/>{' '}
+            &nbsp;<FontAwesomeIcon icon="microphone" size="sm"/>&nbsp;
             <Link className={styles.link}
                   to={buildPath(props.minutes.id)}>{props.minutes.name}</Link>
             で発言しました
@@ -21,26 +21,30 @@ export const MinutesActivityCard = (props) => {
                 .reduce((prev, curr) => [prev, '、', curr])}）
             </span>
             }
-            <br />
-            {props.keyphrases !== null && '　' + props.keyphrases.join('、')}
+            <br/>
+            <span className={styles.keyphrases}>
+                {props.keyphrases !== null && '　' + props.keyphrases.join('、')}
+            </span>
         </div>
-    </p>)
+    </div>)
 }
 
 export const BillActivityCard = (props) => {
-    return (<p className={styles.bill_activity}>
+    return (<div className={styles.activity}>
         {formatDate(props.datetime)}
-        {' '}<FontAwesomeIcon icon="sticky-note" size="sm"/>{' '}
-        <Link className={styles.link}
-              to={buildPath(props.bill.id)}>{props.bill.name}</Link>
-        を提出しました
-        {props.urls.length > 0 &&
-        <span>
-            （{props.urls
-            .map(url => <a className={styles.link} href={url.url} target="_blank"
-                           rel="noopener noreferrer">{url.title}</a>)
-            .reduce((prev, curr) => [prev, '、', curr])}）
-        </span>
-        }
-    </p>)
+        <div>
+            &nbsp;<FontAwesomeIcon icon="sticky-note" size="sm"/>&nbsp;
+            <Link className={styles.link}
+                  to={buildPath(props.bill.id)}>{props.bill.name}</Link>
+            を提出しました
+            {props.urls.length > 0 &&
+            <span>
+                （{props.urls
+                .map(url => <a className={styles.link} href={url.url} target="_blank"
+                               rel="noopener noreferrer">{url.title}</a>)
+                .reduce((prev, curr) => [prev, '、', curr])}）
+            </span>
+            }
+        </div>
+    </div>)
 }
