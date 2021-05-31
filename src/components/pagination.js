6f1dc124a0f851345934563d2ed1ff9c514e83bf
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-06-01 00:15:08 +0900

    POL-314: add pagination

---
+++ src/components/pagination.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import "./pagination.css";
+
+export default function Pagination(props) {
+    return (
+        <ReactPaginate
+            previousLabel={'前へ'}
+            nextLabel={'次へ'}
+            breakLabel={'...'}
+            pageCount={props.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={props.onPageChange}
+            containerClassName={'paginationClassName'}
+            activeClassName={'activeClassName'}
+            breakClassName={'breakClassName'}
+        />);
+}
